fa8c3b9addf58ccde69d037d33805393bca3aede
MASAAKI FURUKI <masaakifuruki@gmail.com>
2016-05-09 10:33:41 -0700

    Add title test and guard test

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'home' => 'welcome#index'
+  get 'about' => 'static_pages#about'
+  get 'terms' => 'static_pages#terms'
+  get 'privacy' => 'static_pages#privacy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Employee Success 345 Thrive"
   end
 
   test "should get terms" do
     get :terms
     assert_response :success
+    assert_select "title", "Terms | Employee Success 345 Thrive"
   end
 
   test "should get privacy" do
     get :privacy
     assert_response :success
+    assert_select "title", "Privacy | Employee Success 345 Thrive"
   end
 
 end
