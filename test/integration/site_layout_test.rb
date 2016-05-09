df94b5792d2c8a7fc6c0405b76b7ee528db78b29
MASAAKI FURUKI <masaakifuruki@gmail.com>
2016-05-09 11:55:48 -0700

    Initialized static pages

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test  "layout links" do
+  	get root_path
+  	assert_template 'welcome/index'
+  	assert_select "a[href=?]", about_path
+  end
+end
