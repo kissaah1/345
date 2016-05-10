bb88a6d155c6121374b7ff3cc9b6cb2a2507e480
MASAAKI FURUKI <masaakifuruki@gmail.com>
2016-05-10 11:57:46 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-  def new
-  end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+			if @user.save
+			# UserMailer.account_activation(@user).deliver_now
+			# @user.send_activation_email
+			flash[:info] = "Please check your email to activate your account."
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+# Confirms a logged-in user.
+#	def logged_in_user
+#		unless logged_in?
+#			store_location
+#			flash[:danger] = "Please log in to do that."
+#			redirect_to login_url
+#		end
+#	end
+
 end
